6295ea4875e838cbfc5df1adde4d32741548c990
Bibash Japrel <bibashjaprel111@gmail.com>
2024-10-29 12:26:30 +0545

    basic backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// Backend entry point
+const express = require('express');
+const connectDB = require('./dbconfig/dbConfig');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Connect to the database
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "This is the dev environment." });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
